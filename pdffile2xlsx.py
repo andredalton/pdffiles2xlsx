2ce8d7e470d6cc86a91cb4dd6a7475c5e0209d67
André Meneghelli Vale <andredalton@gmail.com>
2021-03-01 12:35:00 -0300

    Realizando o tratamento do path.

--- pdffile2xlsx.py
+++ pdffile2xlsx.py
@@ -1,8 +1,10 @@
 import argparse
+import glob
 
 
 def main(args):
-    pass
+    path = args.path if args.path is not None else "."
+    print(glob.glob(path + "/*.pdf"))
 
 
 if __name__ == "__main__":
@@ -10,4 +12,5 @@ if __name__ == "__main__":
     parser.add_argument("--recursive", help="Search PDFs recursively", action="store_true")
     parser.add_argument("--path", help="Chose the origin directory", type=str)
     parser.add_argument("--out", help="Select the xlsx's output name", type=str)
+    args = parser.parse_args()
     main(args)
