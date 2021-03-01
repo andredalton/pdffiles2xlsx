c3be9da526065c5611aa89a5a630a9187f3d747a
André Meneghelli Vale <andredalton@gmail.com>
2021-03-01 16:22:20 -0300

    Inserindo diretório temporário e possibilidade de trabalhar com extensões diferentes.

--- pdffile2xlsx.py
+++ pdffile2xlsx.py
@@ -1,16 +1,27 @@
+from pdf2image import convert_from_path
+import tempfile
 import argparse
 import glob
+import os
 
 
 def main(args):
     path = args.path if args.path is not None else "."
-    print(glob.glob(path + "/*.pdf"))
+    extension = args.extension if args.extension is not None else "pdf"
+    fmt = "png"
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        for fname in glob.glob(path + "/*." + extension):
+            print(fname)
+            file_name = os.path.split(fname)[1]
+            final_name = tmpdirname + "/" + file_name.replace(extension, fmt)
+            convert_from_path(fname, 350, fmt=fmt, output_file=final_name)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--recursive", help="Search PDFs recursively", action="store_true")
     parser.add_argument("--path", help="Chose the origin directory", type=str)
+    parser.add_argument("--extension", help="Allow to change the extensions", type=str)
     parser.add_argument("--out", help="Select the xlsx's output name", type=str)
     args = parser.parse_args()
     main(args)
